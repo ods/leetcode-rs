6c92eff4ae117d745539a97a2b7f0ae052b32792
Denis Otkidach <denis.otkidach@gmail.com>
2021-01-31 15:33:42 +0300

    Week 5, Jan 31: Next Permutation (functional style)

--- src/challenge_2021_jan/week5/mod.rs
+++ src/challenge_2021_jan/week5/mod.rs
@@ -1,2 +1,3 @@
 mod minimum_deviation;
+mod next_permutation;
 mod vertical_traversal;
---
+++ src/challenge_2021_jan/week5/next_permutation.rs
@@ -0,0 +1,78 @@
+// Next Permutation
+// https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/583/week-5-january-29th-january-31st/3623/
+
+struct Solution;
+
+impl Solution {
+    pub fn next_permutation(nums: &mut Vec<i32>) {
+        if nums.len() < 2 {
+            return;
+        }
+
+        // https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+        // Find the largest index k such that a[k] < a[k + 1]
+        let k = match nums
+            .windows(2)
+            .enumerate()
+            .rfind(|(_, pair)| pair[0] < pair[1])
+        {
+            None => {
+                // If no such index exists, the permutation is the last
+                // permutation
+                nums.reverse();
+                return;
+            }
+            Some((idx, _)) => idx,
+        };
+
+        // Find the largest index l greater than k such that a[k] < a[l]
+        let l = k
+            + 1
+            + nums[k + 1..]
+                .iter()
+                .enumerate()
+                .rfind(|(_, &val)| nums[k] < val)
+                .unwrap()
+                .0;
+        dbg!(k, l);
+
+        // Swap the value of a[k] with that of a[l]
+        nums.swap(k, l);
+
+        // Reverse the sequence from a[k + 1] up to and including the final
+        // element a[n]
+        nums[k + 1..].reverse();
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let mut nums = vec![1, 2, 3];
+        Solution::next_permutation(&mut nums);
+        assert_eq!(nums, vec![1, 3, 2]);
+    }
+
+    #[test]
+    fn example2() {
+        let mut nums = vec![3, 2, 1];
+        Solution::next_permutation(&mut nums);
+        assert_eq!(nums, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn example3() {
+        let mut nums = vec![1, 1, 5];
+        Solution::next_permutation(&mut nums);
+        assert_eq!(nums, vec![1, 5, 1]);
+    }
+
+    #[test]
+    fn example4() {
+        let mut nums = vec![1];
+        Solution::next_permutation(&mut nums);
+        assert_eq!(nums, vec![1]);
+    }
+}
