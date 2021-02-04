48e105319cdebdd10869c3138223ee8a497dd09f
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-04 14:17:22 +0300

    Feb 4: Longest Harmonious Subsequence

---
+++ src/challenge_2021_feb/week1/find_lhs.rs
@@ -0,0 +1,53 @@
+// Longest Harmonious Subsequence
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3628/
+
+struct Solution;
+
+impl Solution {
+    pub fn find_lhs(mut nums: Vec<i32>) -> i32 {
+        nums.sort_unstable();
+        let mut max_len = 0;
+        let mut prev_num = nums[0];
+        let mut cur_num = prev_num;
+        let mut prev_len = 0;
+        let mut cur_len = 1;
+        for &num in &nums[1..] {
+            if num == cur_num {
+                cur_len += 1;
+            } else {
+                if (cur_num - prev_num).abs() == 1
+                    && prev_len + cur_len > max_len
+                {
+                    max_len = prev_len + cur_len;
+                }
+                prev_num = cur_num;
+                cur_num = num;
+                prev_len = cur_len;
+                cur_len = 1;
+            }
+        }
+        if (cur_num - prev_num).abs() == 1 && prev_len + cur_len > max_len {
+            max_len = prev_len + cur_len;
+        }
+        max_len
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        assert_eq!(Solution::find_lhs(vec![1, 3, 2, 2, 5, 2, 3, 7]), 5);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(Solution::find_lhs(vec![1, 2, 3, 4]), 2);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(Solution::find_lhs(vec![1, 1, 1, 1]), 0);
+    }
+}
--- src/challenge_2021_feb/week1/mod.rs
+++ src/challenge_2021_feb/week1/mod.rs
@@ -1,3 +1,4 @@
+mod find_lhs;
 mod hamming_weight;
 mod has_cycle;
 mod trim_bst;
