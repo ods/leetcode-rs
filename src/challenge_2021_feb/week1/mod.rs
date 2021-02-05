0b66c8b38dfcae7f6a39135021f9af8954bc4a6e
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-05 14:35:28 +0300

    Feb 5: Simplify Path

--- src/challenge_2021_feb/week1/mod.rs
+++ src/challenge_2021_feb/week1/mod.rs
@@ -1,4 +1,5 @@
 mod find_lhs;
 mod hamming_weight;
 mod has_cycle;
+mod simplify_path;
 mod trim_bst;
---
+++ src/challenge_2021_feb/week1/simplify_path.rs
@@ -0,0 +1,64 @@
+struct Solution;
+
+use std::path::{
+    Component::{Normal, ParentDir},
+    Path,
+};
+
+impl Solution {
+    pub fn simplify_path(path: String) -> String {
+        let mut parts = Vec::new();
+        for component in Path::new(&path).components() {
+            match component {
+                Normal(s) => {
+                    parts.push(s.to_str().unwrap());
+                }
+                ParentDir => {
+                    parts.pop();
+                }
+                _ => {}
+            }
+        }
+        if parts.is_empty() {
+            String::from("/")
+        } else {
+            let mut res = String::new();
+            for part in parts {
+                res.push('/');
+                res.push_str(part)
+            }
+            res
+        }
+    }
+}
+
+mod test {
+    use super::*;
+
+    fn check(src: &str, expected: &str) {
+        assert_eq!(
+            Solution::simplify_path(String::from(src)),
+            String::from(expected)
+        );
+    }
+
+    #[test]
+    fn example1() {
+        check("/home/", "/home");
+    }
+
+    #[test]
+    fn example2() {
+        check("/../", "/");
+    }
+
+    #[test]
+    fn example3() {
+        check("/home//foo/", "/home/foo");
+    }
+
+    #[test]
+    fn example4() {
+        check("/a/./b/../../c/", "/c");
+    }
+}
