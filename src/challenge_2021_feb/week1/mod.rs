e6e931cc9196ff104388377431820afa9f5578c8
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-07 11:47:15 +0300

    Feb 7: Shortest Distance to a Character

--- src/challenge_2021_feb/week1/mod.rs
+++ src/challenge_2021_feb/week1/mod.rs
@@ -2,5 +2,6 @@ mod find_lhs;
 mod hamming_weight;
 mod has_cycle;
 mod right_side_view;
+mod shortest_to_char;
 mod simplify_path;
 mod trim_bst;
---
+++ src/challenge_2021_feb/week1/shortest_to_char.rs
@@ -0,0 +1,55 @@
+// Shortest Distance to a Character
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3631/
+
+struct Solution;
+
+impl Solution {
+    pub fn shortest_to_char(s: String, c: char) -> Vec<i32> {
+        let max_dist = s.len() as i32;
+        let mut res = Vec::with_capacity(s.len());
+        let mut cur_dist = max_dist;
+        for (idx, cur_c) in s.chars().enumerate() {
+            if cur_c == c {
+                cur_dist = 0;
+                let mut back_dist = 1;
+                for back_idx in (0..idx).rev() {
+                    if res[back_idx] <= back_dist {
+                        break;
+                    }
+                    res[back_idx] = back_dist;
+                    back_dist += 1;
+                }
+            } else {
+                cur_dist += 1;
+            }
+            res.push(cur_dist);
+        }
+        res
+    }
+}
+
+mod test {
+    use super::*;
+
+    fn check(s: &str, c: char, expected: &[i32]) {
+        assert_eq!(
+            Solution::shortest_to_char(String::from(s), c),
+            expected.to_vec()
+        )
+    }
+
+    #[test]
+    fn example1() {
+        check("loveleetcode", 'e', &[3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]);
+    }
+
+    #[test]
+    fn example2() {
+        check("aaab", 'b', &[3, 2, 1, 0]);
+    }
+
+    #[test]
+    fn test1() {
+        check(&"a".repeat(10000), 'a', &[0; 10000]);
+    }
+}
