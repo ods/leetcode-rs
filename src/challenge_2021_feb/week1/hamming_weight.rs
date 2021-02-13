e9e0bd24d47f99320ba4c24cf7cc945fedffdc44
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-13 18:20:13 +0300

    Discover u32::count_ones() as cheating solution

--- src/challenge_2021_feb/week1/hamming_weight.rs
+++ src/challenge_2021_feb/week1/hamming_weight.rs
@@ -3,6 +3,15 @@
 
 struct Solution;
 
+// Cheating
+#[cfg(disable)]
+impl Solution {
+    #[allow(non_snake_case)]
+    pub fn hammingWeight(n: u32) -> i32 {
+        n.count_ones() as _
+    }
+}
+
 impl Solution {
     #[allow(non_snake_case)]
     pub fn hammingWeight(mut n: u32) -> i32 {
