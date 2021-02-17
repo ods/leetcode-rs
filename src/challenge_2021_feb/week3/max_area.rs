b26b99be6b6e943627ad3686dae39355d4d0336d
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-17 12:11:21 +0300

    Feb 17: Container With Most Water

---
+++ src/challenge_2021_feb/week3/max_area.rs
@@ -0,0 +1,48 @@
+// Container With Most Water
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3643/
+
+struct Solution;
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut low = 0;
+        let mut high = height.len() - 1;
+        let get_area = |low: usize, high: usize| {
+            height[low].min(height[high]) * (high - low) as i32
+        };
+        let mut max_area = get_area(low, high);
+        while low < high {
+            if height[low] < height[high] {
+                low += 1;
+            } else {
+                high -= 1;
+            }
+            max_area = max_area.max(get_area(low, high));
+        }
+        max_area
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        assert_eq!(Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(Solution::max_area(vec![1, 1]), 1);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(Solution::max_area(vec![4, 3, 2, 1, 4]), 16);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(Solution::max_area(vec![1, 2, 1]), 2);
+    }
+}
--- src/challenge_2021_feb/week3/mod.rs
+++ src/challenge_2021_feb/week3/mod.rs
@@ -1,2 +1,3 @@
 mod k_weakest_rows;
 mod letter_case_permutation;
+mod max_area;
