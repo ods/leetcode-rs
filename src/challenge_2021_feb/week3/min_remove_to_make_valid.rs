1a0e3fd444fa904ba2a4d1a9ee5be14a7b05821a
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-19 14:20:28 +0300

    Feb 19: Minimum Remove to Make Valid Parentheses

---
+++ src/challenge_2021_feb/week3/min_remove_to_make_valid.rs
@@ -0,0 +1,93 @@
+// Minimum Remove to Make Valid Parentheses
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3645/
+
+struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn min_remove_to_make_valid(s: String) -> String {
+        let mut openned = Vec::new();
+        let mut to_drop = HashSet::new();
+        for (idx, c) in s.chars().enumerate() {
+            match c {
+                '(' => {
+                    openned.push(idx);
+                }
+                ')' => {
+                    if openned.pop().is_none() {
+                        to_drop.insert(idx);
+                    }
+                }
+                _ => {}
+            }
+        }
+        while let Some(idx) = openned.pop() {
+            to_drop.insert(idx);
+        }
+        s.chars()
+            .enumerate()
+            .filter_map(|(idx, c)| {
+                if to_drop.contains(&idx) {
+                    None
+                } else {
+                    Some(c)
+                }
+            })
+            .collect()
+    }
+}
+
+mod test {
+    use super::*;
+
+    fn is_valid(s: &str) -> bool {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match c {
+                ')' => {
+                    if stack.pop() != Some('(') {
+                        return false;
+                    }
+                }
+                '(' => {
+                    stack.push(c);
+                    continue;
+                }
+                _ => {}
+            };
+        }
+        stack.is_empty()
+    }
+
+    fn check(s: &str, removed: usize) {
+        let res = Solution::min_remove_to_make_valid(String::from(s));
+        assert!(is_valid(&res));
+        assert_eq!(res.len(), s.len() - removed);
+    }
+
+    #[test]
+    fn example1() {
+        check("lee(t(c)o)de)", 1);
+    }
+
+    #[test]
+    fn example2() {
+        check("a)b(c)d", 1);
+    }
+
+    #[test]
+    fn example3() {
+        check("))((", 4);
+    }
+
+    #[test]
+    fn example4() {
+        check("(a(b(c)d)", 1);
+    }
+
+    #[test]
+    fn test_overlap() {
+        check("a)b(c(d)e", 2);
+    }
+}
--- src/challenge_2021_feb/week3/mod.rs
+++ src/challenge_2021_feb/week3/mod.rs
@@ -1,4 +1,5 @@
 mod k_weakest_rows;
 mod letter_case_permutation;
 mod max_area;
+mod min_remove_to_make_valid;
 mod number_of_arithmetic_slices;
