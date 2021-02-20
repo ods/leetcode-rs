0af89225ade0e84d6e525991faf22fad702ac99a
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-20 11:50:52 +0300

    Feb 20: Roman to Integer

--- src/challenge_2021_feb/week3/mod.rs
+++ src/challenge_2021_feb/week3/mod.rs
@@ -3,3 +3,4 @@ mod letter_case_permutation;
 mod max_area;
 mod min_remove_to_make_valid;
 mod number_of_arithmetic_slices;
+mod roman_to_int;
---
+++ src/challenge_2021_feb/week3/roman_to_int.rs
@@ -0,0 +1,54 @@
+// Roman to Integer
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3646/
+
+struct Solution;
+
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        s.chars()
+            .rev()
+            .map(|c| match c {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => panic!("Invalid numeral"),
+            })
+            .fold((0, 0), |(prev, sum), val| {
+                (val, sum + if val < prev { -val } else { val })
+            })
+            .1
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        assert_eq!(Solution::roman_to_int(String::from("III")), 3);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(Solution::roman_to_int(String::from("IV")), 4);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(Solution::roman_to_int(String::from("IX")), 9);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(Solution::roman_to_int(String::from("LVIII")), 58);
+    }
+
+    #[test]
+    fn example5() {
+        assert_eq!(Solution::roman_to_int(String::from("MCMXCIV")), 1994);
+    }
+}
