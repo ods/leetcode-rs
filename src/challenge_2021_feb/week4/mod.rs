30354167b954055ea8f5b3dcecbd7ca1a8e40f4b
Denis Otkidach <denis.otkidach@gmail.com>
2021-02-26 12:56:24 +0300

    Feb 26: Validate Stack Sequences

--- src/challenge_2021_feb/week4/mod.rs
+++ src/challenge_2021_feb/week4/mod.rs
@@ -2,3 +2,4 @@ mod find_longest_word;
 mod find_unsorted_subarray;
 mod score_of_parentheses;
 mod search_matrix;
+mod validate_stack_sequences;
---
+++ src/challenge_2021_feb/week4/validate_stack_sequences.rs
@@ -0,0 +1,55 @@
+// Validate Stack Sequences
+// https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3653/
+
+struct Solution;
+
+impl Solution {
+    pub fn validate_stack_sequences(
+        pushed: Vec<i32>,
+        popped: Vec<i32>,
+    ) -> bool {
+        let mut push_it = pushed.iter();
+        let mut pop_it = popped.iter().peekable();
+        let mut stack = Vec::new();
+        while let Some(&val) = push_it.next() {
+            stack.push(val);
+            loop {
+                match stack.last() {
+                    Some(last) if pop_it.peek() == Some(&last) => {
+                        stack.pop();
+                        pop_it.next();
+                    }
+                    _ => break,
+                }
+            }
+        }
+        stack.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        assert_eq!(
+            Solution::validate_stack_sequences(
+                vec![1, 2, 3, 4, 5],
+                vec![4, 5, 3, 2, 1],
+            ),
+            true,
+        );
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(
+            Solution::validate_stack_sequences(
+                vec![1, 2, 3, 4, 5],
+                vec![4, 3, 5, 1, 2],
+            ),
+            false,
+        );
+    }
+}
